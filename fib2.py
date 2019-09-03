5246a7ba1c994fca1cbb5c3d895b4f7a4ac4370a
yusuke hara <hara.yusuke1998@gmail.com>
2019-09-03 13:25:48 +0900

    fib2

---
+++ fib2.py
@@ -0,0 +1,15 @@
+﻿def fib(m):
+        if n <= 2:
+                return 1
+        else:
+                return fib(n-1) + fib(n-2)
+               
+                
+result = []
+
+for n in range(1,20):
+        result.append(fib(n))
+
+
+print(result)
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
