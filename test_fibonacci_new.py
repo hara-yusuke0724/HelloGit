0c9486479648e87c8bc220f031b0c22ef92622fb
yusuke hara <hara.yusuke1998@gmail.com>
2019-09-04 14:12:31 +0900

    フィボナッチ数列のプログラムや単体テストを別のアルゴリズムで作成

---
+++ test_fibonacci_new.py
@@ -0,0 +1,24 @@
+import unittest
+import fibonacci
+
+class Testfibobacci(unittest.TestCase):
+    def test_fibobacci_new(self):
+        pairs = [
+              [0, 0],
+              [1, 0],
+              [2, 1],
+              [3, 1],
+              [4, 2],
+              [5, 3],
+              [6, 5],
+              [7, 8],
+              [8, 13],
+              [9, 21],
+              [10, 34],
+
+            ]
+
+                
+
+if __name__ == "__main__":
+    unittest.main()
