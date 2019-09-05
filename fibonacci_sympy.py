98e946df2788fafeeaa52936eb6bbc0931c772b2
yusuke hara <hara.yusuke1998@gmail.com>
2019-09-05 10:45:12 +0900

    フィボナッチ数列のプログラムを一般項の求解で書いたプログラム

---
+++ fibonacci_sympy.py
@@ -0,0 +1,12 @@
+import sympy  #sympyのインポート
+def Fib(n):
+    x = sympy.symbols('x',nonnegative=True,integer=True) #変数xの宣言(非負の整数)
+    Fib=1/sympy.sqrt(5)*(((1+sympy.sqrt(5))/2)**(n-1)-((1-sympy.sqrt(5))/2)**(n-1)) #一般項の式
+    result=Fib.subs(x,n) #xにnを代入
+    result=result.evalf() #式を浮動小数点数として評価
+    return int(result)
+Fiblist=[]
+for n in range(1,10):
+    Fiblist+=[Fib(n)]
+print(Fiblist)
+# [0, 1, 1, 2, 3, 5, 8, 13, 21]
---
+++ test_fibonacci_sympy.py
@@ -0,0 +1,30 @@
+import unittest
+import fibonacci
+import sympy
+
+class Testfibonacci(unittest.TestCase):
+    def test_fibonacci_sympy(self):
+        self.assertEqual(fibonacci.Fibonacci(0), 0)
+        self.assertEqual(fibonacci.Fibonacci(1), 0)
+        self.assertEqual(fibonacci.Fibonacci(2), 1)
+        self.assertEqual(fibonacci.Fibonacci(3), 1)
+        self.assertEqual(fibonacci.Fibonacci(4), 2)
+        self.assertEqual(fibonacci.Fibonacci(5), 3)
+        self.assertEqual(fibonacci.Fibonacci(6), 5)
+        self.assertEqual(fibonacci.Fibonacci(7), 8)
+        self.assertEqual(fibonacci.Fibonacci(8), 13)
+        self.assertEqual(fibonacci.Fibonacci(9), 21)
+        self.assertEqual(fibonacci.Fibonacci(10), 34)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
